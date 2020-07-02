2067484593ae3ecc7dd5d78db7e8c6ead0f9f29b
Oscar Quintanilla <38816404+owqh@users.noreply.github.com>
2020-07-01 19:37:23 -0600

    Prueba de comit

--- src/com/bitlab/principal/Proyecto4.java
+++ src/com/bitlab/principal/Proyecto4.java
@@ -15,7 +15,7 @@ public class Proyecto4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hasdjasda");
     }
     
 }
