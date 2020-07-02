4796e473003b95869575959771be9065850a7b47
Carlos Godoy <carlosgodoy576@gmail.com>
2020-07-02 17:51:00 -0600

    creando la conexion por medio de socket, mostrando menu inicial para los clientes que se conectaran

--- src/com/bitlab/principal/Proyecto4.java
+++ src/com/bitlab/principal/Proyecto4.java
@@ -10,9 +10,15 @@ import com.bitlab.dao.EmpleadosDAO;
 import com.bitlab.datos.correo.DatosCorreo;
 import com.bitlab.encriptador.Encriptador;
 import com.bitlab.entidades.Empleado;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Properties;
 import java.util.Scanner;
 import java.util.logging.Level;
 import org.slf4j.Logger;
@@ -28,44 +34,85 @@ public class Proyecto4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-  Encriptador encriptar = new Encriptador();
-        DatosCorreo datosCorreo = new DatosCorreo();
-        Scanner sc = new Scanner(System.in);
-        System.out.println("esto es un texto encriptado su texto original es 'hola mundo' ");
-        System.out.println(encriptar.getTextoEncriptado("hola mundo"));
-        
-        System.out.println("Desencriptando texto");
-        System.out.println(encriptar.getTextoDesencriptado("LthK015TeUjwccjt4Nm7j91rJp0dcjhhMnJC6wCSZpI="));
-        
-        System.out.println("ingrese el correo");
-        datosCorreo.setCorreo(sc.nextLine());
-        logger.debug("se esta enviado el correo a datosCorreo");
-        System.out.println("escriba el mensaje de su correo");
-        datosCorreo.setMensaje(sc.nextLine());
-        logger.debug("se esta enviado el mensaje a datosCorreo");
-        logger.debug("se empieza con el proceso de envio de correo");
-        Correo.enviarCorreo(datosCorreo);
-        System.out.println("correo enviado");
-       
+          //instancias
+        Properties prop = new Properties();
+        Encriptador encriptar = new Encriptador();
+        String opcionCliente = "";
 
-        EmpleadosDAO empDao = new EmpleadosDAO();
-        logger.debug("mostrando el select de la consulta");
         try {
-            List<Empleado> empleado = empDao.obtenerTodoDatos();
-            for(Empleado emp: empleado){
-                System.out.println(emp.toString());
-            }
-            
-            
-            
-            
-            
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Proyecto4.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SQLException ex) {
+            ServerSocket socketServidor = new ServerSocket(8888);
+            Socket socketCliente = null;
+            PrintWriter salida = null;
+            BufferedReader entrada = null;
+            boolean bandera = true;
+            do {
+                logger.debug("EL servidor espera conexion en el puerto 8888");
+                System.out.println("El servidor espera conexion en el puerto 8888");
+                socketCliente = socketServidor.accept();
+                logger.debug("El cliente ha sido aceptado satisfactoriamente");
+                entrada = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
+                salida = new PrintWriter(socketCliente.getOutputStream(), true);
+                
+                boolean banderaOpciones = true;
+                salida.println("Bienvenido clliente\n\r ");
+
+                while (banderaOpciones) {
+                    salida.println(" **** ¡Servidor BitLab.-Echotech Elija la opcion que desee! **** \n\r");
+                    salida.println(" ||------------------------------------------- ||\n\r");
+                    salida.println(" || 1. Seleccionar empleados                   ||\n\r");
+                    salida.println(" || 2.                                         ||\n\r");
+                    salida.println(" || 3.                                         ||\n\r");
+                    salida.println(" || 4.                                         ||\n\r");
+                    salida.println(" || 5. salir                                   ||\n\r");
+                    salida.println(" || apagar (Digite la palabra exactamente)     ||\n\r");
+                    salida.println(" || -------------------------------------------||\n\r");
+
+                    if ((opcionCliente = entrada.readLine()) != null) {
+
+                        switch (opcionCliente) {
+                            case "1":
+                                salida.println("mostrando select");
+                                EmpleadosDAO empDao = new EmpleadosDAO();
+                                logger.debug("mostrando el select de la consulta \n\r");
+
+                                List<Empleado> empleado = empDao.obtenerTodoDatos();
+                                for (Empleado emp : empleado) {
+                                    System.out.println(emp.toString());
+                                    salida.println(emp.toString() + "\n\r");
+                                }
+
+                                break;
+                            case "5":
+                                salida.print("saliendo del sistema");
+                                banderaOpciones = false;
+                                break;
+                            case "apagar":
+                                logger.debug("se esta apagando el servidor");
+                                salida.println("esta apagando el servidor \n\r");
+                                logger.debug("el cliente ha apagado el servidor y el correo ha sido enviado al administrador");
+
+                                banderaOpciones = false;
+                                 break;
+                            default:
+                                salida.println("comando desconocido vuelva a escribirlo \n\r");
+                        }
+
+                    }
+                    Thread.sleep(1000);
+
+                }
+                entrada.close();
+                salida.close();
+                socketCliente.close();
+
+            } while (!"apagar".equals(opcionCliente));
+            logger.debug("¡APAGANDO EL SERVIDOR!");
+            System.out.println(" el cliente: " + socketCliente.getInetAddress() + " esta apagando el servidor");
+            //socketServidor y la instruccion close, hacen que el servidor se apague y finalice el proyecto.
+            socketServidor.close();
+        } catch (IOException | InterruptedException | ClassNotFoundException | SQLException ex) {
             java.util.logging.Logger.getLogger(Proyecto4.class.getName()).log(Level.SEVERE, null, ex);
         }
-    
 
     }
     
