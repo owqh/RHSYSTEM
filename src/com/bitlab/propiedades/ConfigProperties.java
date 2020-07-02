622baffe463b3519627e6aa2d34b5a1ce9771e4c
Cognizant32-12 <Cognizant32-12@192.168.0.12>
2020-07-02 03:16:47 -0500

    Creacion de metodo para obtener las la informacion del archivo properties

--- src/com/bitlab/propiedades/ConfigProperties.java
+++ src/com/bitlab/propiedades/ConfigProperties.java
@@ -5,13 +5,21 @@
  */
 package com.bitlab.propiedades;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author carlosGodoy
  */
 public class ConfigProperties {
+    
+    private Properties propSistema;
+    private final String ARCHIVO_PROPIEDADES = "propiedades.properties";
+    
     /*esta clase de propiedades se usa como un pivote
         se usa para poder leer la informacion que este dentro del paquete de propiedades
         se obtiene un recurso como un entrada de Stream
@@ -27,5 +35,20 @@ public class ConfigProperties {
         el inputStream es una entrada de datos en este caso como en este paque
         tenemos datos este lo hace el inputStream
         */
+    }
+    //Metodo para cargar las informacion del archivo properties
+    public void cargarPropiedades (){
+        try {
+            propSistema = new Properties();
+            propSistema.load(ConfigProperties.getResourceAsInputStream(ARCHIVO_PROPIEDADES));
+        } catch (IOException ex) {
+            Logger.getLogger(ConfigProperties.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+    }
+
+    public Properties getPropSistema() {
+        return propSistema;
     }
 }
